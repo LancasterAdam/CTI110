3b2b178afd3362dc15ac2b60569ca546f385f76a
Lncstr-dev <62820528+Lncstr-dev@users.noreply.github.com>
2020-04-01 23:40:36 -0400

    Add files via upload

--- P4T1a_Lancaster.py
+++ P4T1a_Lancaster.py
@@ -4,3 +4,17 @@
 # 4/1/20
 #
 
+import turtle                               # import turtle package.
+
+size = 10                                   # set size to 5. 
+
+turtle.showturtle()                         # show the turtle.
+turtle.left(180)                            # orient turtle properly to start.
+turtle.speed(0)                             # set turtle speed.
+
+for x in range (100):                       # make 100 shapes.
+    for y in range (4):                     # make shape 4 sided.
+        turtle.forward (size)               # size of shape.
+        turtle.right (90)                   # angle of corner.
+    else:                                   # at end of loop, increase size.
+        size += 3                           # set increment to increase by.
