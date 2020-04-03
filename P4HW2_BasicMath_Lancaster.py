de792a1cb228870f508b5a88857840fb144237be
Lncstr-dev <62820528+Lncstr-dev@users.noreply.github.com>
2020-04-03 03:01:44 -0400

    check before submission

---
+++ P4HW2_BasicMath_Lancaster.py
@@ -0,0 +1,48 @@
+# CTI-110
+# P4HW2 - BasicMath
+# Adam Lancaster
+# 4/3/20
+
+repeat1 = 'y'                                           # set repeat flag 1.
+repeat2 = 'y'                                           # set repeat flag 2.
+
+while repeat1 == 'y':                                   # check for repeat flag.
+    
+    number1 = int(input('Enter first number: '))        # input for number 1.
+    number2 = int(input('Enter second number: '))       # input for number 2.
+
+    while repeat2 == 'y':                               # check for repeat flag.
+        
+        print('--------MENU--------')                   # display menu.
+        print('1) Add Numbers')
+        print('2) Multiply Numbers')
+        print('3) Subtract Numbers')
+        print('4) Exit')
+        print('--------------------')
+                                                        # input for formula.
+        math = str(input('Choose function: (1/2/3/4): '))
+
+        if ((math == '1') or (math == '2') or +         # validate entry.
+            (math == '3') or (math == '4')):
+            repeat2 = 'n'
+
+        else:                                           # reject entry. 
+            print('Invalid entry.')
+
+    repeat2 = 'y'                                       # reset flag.
+
+    if math == '1':                                     # addition check.
+        output = number1 + number2                      # addition output.
+        print(output)
+
+    elif math == '2':                                   # multiply check.
+        output = number1 * number2                      # multiply output.
+        print(output)
+
+    elif math == '3':                                   # subtract check.
+        output = number1 - number2                      # subtract output.
+        print(output)
+
+    else:                                               # exit check.
+        print('Program will terminate.')                # exit output.
+        repeat1 = 'n'                                   # set repeat flag.
