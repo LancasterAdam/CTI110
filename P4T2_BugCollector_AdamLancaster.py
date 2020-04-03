2625568b71ceadc6b690bcd3661544c96f392508
Lncstr-dev <62820528+Lncstr-dev@users.noreply.github.com>
2020-04-03 00:49:59 -0400

    not final, needs updating

---
+++ P4T2_BugCollector_AdamLancaster.py
@@ -0,0 +1,12 @@
+# Bug Collector
+# 4/2/20
+# CTI-110 P4T2
+# Adam Lancaster
+
+total = int(input('enter day 1 bug total: '))           # get day 1 total.
+total += int(input('enter day 2 total: '))              # add day 2 to total.
+total += int(input('enter day 3 total: '))              # add day 3 total.
+total += int(input('enter day 4 total: '))              # add day 4 total.
+total += int(input('enter day 5 total: '))              # add day 5 total.
+
+print('total bugs collected for the week is:', total)
