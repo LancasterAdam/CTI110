5e67e641c3fd7d20419454124b313aa536dbdf26
Lncstr-dev <62820528+Lncstr-dev@users.noreply.github.com>
2020-04-03 03:35:59 -0400

    Add files via upload

---
+++ P4HW3_NestedLoops_AdamLancaster.py
@@ -0,0 +1,11 @@
+# Nested Loops
+# 4/3/20
+# CTI-110 P4HW3 - Nested Loops
+# Adam Lancaster
+
+for x in range (6):
+    print('#', end='')
+    for y in range(x):
+        print(' ', end='')
+    print('#')
+    
