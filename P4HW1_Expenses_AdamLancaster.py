ce56ae9bc33d9ee9b5e593ed7b12cf57874a5c92
Lncstr-dev <62820528+Lncstr-dev@users.noreply.github.com>
2020-04-03 02:41:47 -0400

    Add files via upload

---
+++ P4HW1_Expenses_AdamLancaster.py
@@ -0,0 +1,33 @@
+# CTI-110
+# P4HW1 - Expenses
+# Adam Lancaster
+# 4/3/20
+
+exp = []                                                # create expense array.
+
+account = float(input('enter account balance: '))       # get account balance.
+
+repeat = 'y'                                            # set repeat flag.
+
+while repeat != 'n':                                    # set input loop.
+
+    exp.append(int(input('enter expense amount: ')))    # add expense to array.
+    repeat = str(input('add another expense? (y/n)'))   # ask for repeat.
+
+expNum = len(exp)                                       # get array length.
+
+expTotal = 0                                            # set exp starting value
+
+while len(exp) > 0:                                     # if array has contents,
+        expTotal += exp[0]                              # read 1st value, add to
+        exp.pop(0)                                      # total, clear entry.
+
+balance = format(account - expTotal, ',.2f')            # calculate balance.
+
+print('starting acount balance: $',
+    format(account, ',.2f'))                            # show starting balance.
+
+print('number of expenses entered:', expNum)            # show # of expenses.
+        
+print('balance after expenses: $', balance)             # show final balance.
+    
